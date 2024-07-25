cb92a19d0f6fd9940a790dc964476a39cda09509
Stubborn Stone <yangyuexin1990@gmail.com>
2024-07-25 23:10:04 +0800

    add osc-52

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,4 +1,3 @@
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -13,5 +12,26 @@ vim.filetype.add {
   },
   pattern = {
     ["~/%.config/foo/.*"] = "fooscript",
+  },
+}
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg "", "\n"),
+    vim.fn.getregtype "",
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+    ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
   },
 }
